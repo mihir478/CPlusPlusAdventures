3eb7d2af854e81de2e5491f368816e0fc7bb17ca
Mihir Sanghavi <mihir.h.sanghavi@gmail.com>
2021-03-24 22:18:33 -0400

    Step function

--- hello/hello/main.cpp
+++ hello/hello/main.cpp
@@ -1,6 +1,15 @@
 #include <cstdio>
 
-int main() {
-	printf("Hello, World!");
+int step_function(int x) {
+	if (x < 0)
+		return -1;
+	else if (x > 0)
+		return 1;
 	return 0;
+}
+
+int main() {
+	printf("Num1: %d, Step: %d\n", 42, step_function(42));
+	printf("Num2: %d, Step: %d\n", 0, step_function(0));
+	printf("Num3: %d, Step: %d\n", -32767, step_function(-32767));
 }
