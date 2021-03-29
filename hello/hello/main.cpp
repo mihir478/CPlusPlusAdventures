b7b25d3ca5adbc28d4a553fafc32db14c5f6b83a
Mihir Sanghavi <mihir.h.sanghavi@gmail.com>
2021-03-28 20:34:45 -0400

    integer literals

--- hello/hello/main.cpp
+++ hello/hello/main.cpp
@@ -9,8 +9,19 @@ int step_function(int x) {
 }
 
 int main() {
+	/* Chapter 1 */
 	// printf returns the total number of characters printed or negative values for an encoding error
 	printf("Num1: %d, Step: %d\n", 42, step_function(42));
 	printf("Num2: %d, Step: %d\n", 0, step_function(0));
 	printf("Num3: %d, Step: %d\n", -32767, step_function(-32767));
+
+	/* Chapter 2 */
+	printf("\nTypes:\n");
+	unsigned short a = 0b10101010;
+	printf("%hu\n", a);
+	int b = 0123;
+	printf("%d\n", b);
+	unsigned long long d = 0xFFFFFFFFFFFF;
+	printf("%lld\n", d);
+	return 0;
 }
